9b43f27d15bbbe688e4d62c843d5660e4ea9359e
Kevin Stosik <kevin.stosik@zarm.uni-bremen.de>
2024-01-17 11:54:56 +0100

    Initial commit for the KeyCloak implementation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,37 @@
-import { ApplicationConfig } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {KeycloakService} from "keycloak-angular";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'https://keycloak.szut.dev/auth',
+        realm: 'szut',
+        clientId: 'employee-management-service-frontend'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    })
+      .catch((error) :void => {
+        console.error('Keycloak initialization failed', error)
+      });
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    KeycloakService
+  ]
 };
