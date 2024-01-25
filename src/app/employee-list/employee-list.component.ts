e0a333bc99c601704749b640b806f1b8e0dd32b4
Vrglab <vrglab1@gmail.com>
2024-01-25 12:41:27 +0100

    Rough test of searching for employee's is done

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,25 +1,48 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Observable, of} from "rxjs";
+import {map, Observable, of} from "rxjs";
 import {Employee} from "../Employee";
 import {ExternalEmployeeServiceHandler} from "../services/external-employee-service-handler";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [CommonModule, ExternalEmployeeServiceHandler],
+  imports: [CommonModule, ExternalEmployeeServiceHandler, FormsModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
-  employees$: Observable<Employee[]>;
+  TotalEmployees$: Observable<Employee[]>;
+  displayedEmployees$: Observable<Employee[]>;
+  // @ts-ignore
+  searchedId: number;
+  searchedFirstname: string = '';
+  searchedLastname: string = '';
 
   constructor(private handler: ExternalEmployeeServiceHandler) {
-    this.employees$ = of([]);
+    this.TotalEmployees$ = of([]);
     this.fetchData();
+    this.displayedEmployees$ = this.TotalEmployees$;
   }
 
   fetchData() {
-    this.employees$ = this.handler.requestAllEmployees();
+    this.TotalEmployees$ = this.handler.requestAllEmployees();
+  }
+
+  searchForEmployee() {
+    this.displayedEmployees$ = this.TotalEmployees$.pipe(
+      map(employees => employees.filter(employee => {
+        let matchesId = this.searchedId > -1 ? employee.id === this.searchedId : true;
+        let matchesFirstName = this.searchedFirstname !== '' ? employee.firstName?.includes(this.searchedFirstname) : true;
+        let matchesLastName = this.searchedLastname !== '' ? employee.lastName?.includes(this.searchedLastname) : true;
+
+        return matchesId && matchesFirstName && matchesLastName;
+      }))
+    );
+  }
+
+  ResetDisplay() {
+    this.displayedEmployees$ = this.TotalEmployees$;
   }
 }
